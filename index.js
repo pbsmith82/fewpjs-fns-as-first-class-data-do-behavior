2d75256844e394d66c3285209f054dd0f92055e8
pbsmith82 <pbsmith82@gmail.com>
2021-03-19 13:19:16 -0400

    commit tree 6d5be2b6a24695bf2c968e90d68a7acb35843d0e

--- index.js
+++ index.js
@@ -4,10 +4,27 @@ document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+ 
+   displayMessage(greet(timeString))
 }
+
+const greeting = document.getElementById('greeting')
+
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(ts){
+  let num = parseInt(ts)
+  if (num < 12) {
+    return "Good Morning"
+  } else if (num < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  } 
+  }
 /* Write your implementation of displayMessage() */
+function displayMessage(response) {
+  greeting.innerText = response
+}
